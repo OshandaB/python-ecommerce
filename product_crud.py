8e40211b6cd1f5c54e6d8b880185bd30e0e6bfe2
AmilSrinath <amilsrinath5@gmail.com>
2025-01-25 12:08:08 +0530

    update customer

--- product_crud.py
+++ product_crud.py
@@ -1,4 +1,5 @@
 from product import Product
+from customer import Customer
 
 #create a product object
 product1 = Product("Phone", 34000, 10)
@@ -7,3 +8,6 @@ print(product1.get_details())
 #update the price of the product
 product1.update_price(35000)
 print(product1.get_details())
+
+customer1 = Customer("Jone", "No 3", "+94777777777")
+print(customer1.get_details())
