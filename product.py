3d5bd7424744ee43416ea1f4d803c9089ea5c476
duvindu111 <duvinduthathsara@gmail.com>
2025-01-25 11:31:41 +0530

    product class

--- product.py
+++ product.py
@@ -1,7 +1,14 @@
 class Product:
-    def __init__(self, name, price):
+    def __init__(self, name, price, quantity):
         self.name = name
         self.price = price
+        self.quantity = quantity
 
-    def __str__(self):
-        return f"{self.name} costs {self.price}"
+    def get_details(self):
+        return f"Product: {self.name}, Price: {self.price}, Quantity: {self.quantity}"
+    
+    def update_price(self, new_price):
+        self.price = new_price
+
+    def update_quantity(self, new_quantity):
+        self.quantity = new_quantity
